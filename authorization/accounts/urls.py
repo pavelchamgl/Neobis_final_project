6a6bb85c7117a2b7a05a632ec0abd3268a8a67dd
Pavel <pavel@MacBook-Pro-Pavel.local>
2023-07-13 10:07:08 +0300

    Added send code url.

--- authorization/accounts/urls.py
+++ authorization/accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .endpoints import UserCreateAPIView, PhoneNumberVerificationAPIView, SetBirthDayAPIView
+from .endpoints import UserCreateAPIView, PhoneNumberVerificationAPIView, SetBirthDayAPIView, SendCodeAPIView
 
 
 urlpatterns = [
     path('api/register/', UserCreateAPIView.as_view(), name="register"),
     path('api/phone_number/verify/', PhoneNumberVerificationAPIView.as_view(), name="phone_number_verify"),
     path('api/birth_day/set/', SetBirthDayAPIView.as_view(), name="set_birth_day"),
+    path('api/send_code/', SendCodeAPIView.as_view(), name="send_code"),
 ]
